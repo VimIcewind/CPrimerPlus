5ef0557bdcdca1e78b765e506f561f65117707d7
VimIcewind <VimIcewind@gmail.com>
2020-07-13 16:51:11 +0800

    toes

---
+++ src/ch02/toes.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int toes, toes2, toestoes;
+
+	toes = 10;
+	toes2 = toes * 2;
+	toestoes = toes * toes;
+
+	printf("toes is %d \n", toes);
+	printf("toes2 is %d \n", toes2);
+	printf("toestoes is %d \n", toestoes);
+
+	return 0;
+}
