b85b2509e57ecf6c508d8b345ddfc9b6ce3406d0
VimIcewind <VimIcewind@gmail.com>
2020-07-13 16:29:56 +0800

    Show my name and address. Show age and days too.

---
+++ src/ch02/age2days.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int age, days;
+
+	age = 30;
+	days = 365 * 30;
+	printf("I am %d year's old, and I am always alive %d days.\n", age, days);
+
+	return 0;
+}
