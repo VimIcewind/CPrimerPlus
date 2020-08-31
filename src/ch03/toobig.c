041f9f039c5cd051714258ab44cfac5ef2056c85
VimIcewind <VimIcewind@gmail.com>
2020-08-31 22:32:20 +0800

    toobig

---
+++ src/ch03/toobig.c
@@ -0,0 +1,13 @@
+/* toobig.c -- 超出系统允许最大的int值 */
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	int i = 2147483647;
+	unsigned int j = 4294967295;
+
+	printf("%d %d %d\n", i, i+1, i+2);
+	printf("%u %u %u\n", j, j+1, j+2);
+
+	return 0;
+}
