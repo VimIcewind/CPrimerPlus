ffcb40d733dc688ee0adef817633c3c00a840dd7
VimIcewind <VimIcewind@gmail.com>
2020-09-02 21:44:07 +0800

    badcount

---
+++ src/ch03/badcount.c
@@ -0,0 +1,16 @@
+/* badcount.c -- 参数错误的情况 */
+#include <stdio.h>
+
+int main(void)
+{
+	int n = 4;
+	int m = 5;
+	float f = 7.0f;
+	float g = 8.0f;
+
+	printf("%d\n", n, m);
+	printf("%d %d %d\n", n);
+	printf("%d %d\n", f, g);
+
+	return 0;
+}
