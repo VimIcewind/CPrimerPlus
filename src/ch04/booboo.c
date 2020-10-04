30c4ef96e4fad0807552cfc9406ebbe3212247e3
VimIcewind <VimIcewind@gmail.com>
2020-10-04 18:27:38 +0800

    booboo

---
+++ src/ch04/booboo.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+#define B "booboo"
+#define X 10
+
+int main(void)
+{
+	int age;
+	int xp;
+	char name[40];
+
+	printf("Please enter your first name.\n");
+	scanf("%s", name);
+	printf("All right, %s, what's your age?\n", name);
+	scanf("%d", &age);
+	xp = age + X;
+	printf("That's a %s! You must be at least %d.\n", B, xp);
+
+	return 0;
+}
