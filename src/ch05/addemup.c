9153000dd151841fb76e34490fab1ee1c4914285
VimIcewind <VimIcewind@gmail.com>
2020-10-06 10:24:25 +0800

    addemup

---
+++ src/ch05/addemup.c
@@ -0,0 +1,15 @@
+/* addemup.c -- 几种常见的语句 */
+#include <stdio.h>
+
+int main(void)
+{
+	int count, sum;
+
+	count = 0;
+	sum = 0;
+	while (count++ < 20)
+		sum = sum + count;
+	printf("sum = %d\n", sum);
+
+	return 0;
+}
