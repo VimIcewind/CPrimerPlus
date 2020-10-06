21abdcdeb9f0cc152e1d780fc704ca851d8425e7
VimIcewind <VimIcewind@gmail.com>
2020-10-06 21:10:14 +0800

    summing too

--- src/ch06/summing.c
+++ src/ch06/summing.c
@@ -5,15 +5,12 @@ int main(void)
 {
 	long num;
 	long sum = 0L;
-	int status;
 
 	printf("Please enter an integer to be summed ");
 	printf("(q to quit): ");
-	status = scanf("%ld", &num);
-	while (status == 1) {
+	while (scanf("%ld", &num) == 1) {
 		sum = sum + num;
 		printf("Please enter next integer (q to quit): ");
-		status = scanf("%ld", &num);
 	}
 	printf("Those integers sum to %ld.\n", sum);
 
