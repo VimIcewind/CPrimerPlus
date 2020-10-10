18abadc3a04d717865fde7a1687c7fc78ec5b7ac
VimIcewind <VimIcewind@gmail.com>
2020-10-10 09:42:57 +0800

    Just add space for code style

--- src/ch06/for2.c
+++ src/ch06/for2.c
@@ -4,7 +4,7 @@ int main(void)
 {
 	int value;
 
-	for (value = 36; value >0; value /= 2)
+	for (value = 36; value > 0; value /= 2)
 		printf("%3d", value);
 	printf("\n");
 
--- src/ch06/for2double.c
+++ src/ch06/for2double.c
@@ -4,7 +4,7 @@ int main(void)
 {
 	double value;
 
-	for (value = 36; value >0; value /= 2)
+	for (value = 36; value > 0; value /= 2)
 		printf("%13e\n", value);
 	printf("\n");
 
