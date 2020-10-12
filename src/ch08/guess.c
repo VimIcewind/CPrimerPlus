284514fa113d2766e61ec459e5a7d193143b691c
VimIcewind <VimIcewind@gmail.com>
2020-10-12 15:08:05 +0800

    guess version 3

--- src/ch08/guess.c
+++ src/ch08/guess.c
@@ -4,13 +4,17 @@
 int main(void)
 {
 	int guess = 1;
+	char response;
 
 	printf("Pick an integer from 1 to 100. I will try to guess ");
 	printf("it.\nRespond with a y if my guess is right and with");
 	printf("\nan n if it is wrong.\n");
 	printf("Uh...is your number %d?\n", guess);
-	while (getchar() != 'y') {
-		printf("Well, then, is it %d?\n", ++guess);
+	while ((response = getchar()) != 'y') {
+		if (response == 'n')
+			printf("Well, then, is it %d?\n", ++guess);
+		else
+			printf("Sorry, I understand only y or n.\n");
 		while (getchar() != '\n')
 			continue;
 	}
