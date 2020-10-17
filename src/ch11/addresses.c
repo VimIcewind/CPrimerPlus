00ba7fd8417d0fb8f6fc385fe20ef73206b1354b
VimIcewind <VimIcewind@gmail.com>
2020-10-17 22:35:32 +0800

    addresses

---
+++ src/ch11/addresses.c
@@ -0,0 +1,18 @@
+// addresses.c -- 字符串地址
+# include <stdio.h>
+
+#define MSG "I'm special"
+
+int main(void)
+{
+	char ar[] = MSG;
+	const char *pt = MSG;
+
+	printf("address of \"I'm special\": %p\n", "I'm special");
+	printf("              address ar: %p\n", ar);
+	printf("              address pt: %p\n", pt);
+	printf("          address of MSG: %p\n", MSG);
+	printf("address of \"I'm special\": %p\n", "I'm special");
+
+	return 0;
+}
