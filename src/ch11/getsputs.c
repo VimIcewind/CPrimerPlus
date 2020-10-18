7ff2f72b60c895b806ff0d066bcaacd0f7a15dec
VimIcewind <VimIcewind@gmail.com>
2020-10-18 21:14:11 +0800

    gets() puts()

---
+++ src/ch11/getsputs.c
@@ -0,0 +1,18 @@
+/* getsputs.c -- 使用 gets() 和 puts() */
+#include <stdio.h>
+
+#define STLEN 81
+
+int main(void)
+{
+	char words[STLEN];
+
+	puts("Enter a string, please.");
+	gets(words);
+	printf("Your string twice:\n");
+	printf("%s\n", words);
+	puts(words);
+	puts("Done.");
+
+	return 0;
+}
