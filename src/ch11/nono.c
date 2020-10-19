d20048dbefdc3be65887cc221bf862ba217dadda
VimIcewind <VimIcewind@gmail.com>
2020-10-19 13:14:48 +0800

    nono

---
+++ src/ch11/nono.c
@@ -0,0 +1,13 @@
+/* nono.c -- 千万不要模仿! */
+#include <stdio.h>
+
+int main(void)
+{
+	char side_a[] = "Side A";
+	char dont[] = {'W', 'O', 'W', '!'};
+	char side_b[] = "Side B";
+
+	puts(dont);
+
+	return 0;
+}
