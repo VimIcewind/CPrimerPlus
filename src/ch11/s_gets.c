381746e5a26774a51a6901d45dbff9c520801a3c
VimIcewind <VimIcewind@gmail.com>
2020-10-20 15:50:17 +0800

    s_gets

--- src/ch11/s_gets.c
+++ src/ch11/s_gets.c
@@ -3,14 +3,13 @@
 char *s_gets(char *st, int n)
 {
 	char *ret_val;
-	int i = 0;
 
 	ret_val = fgets(st, n, stdin);
 	if (ret_val) {
-		while (st[i] != '\n' && st[i] != '\0')
-			i++;
-		if (st[i] == '\n')
-			st[i] = '\0';
+		while (*st != '\n' && *st != '\0')
+			st++;
+		if (*st == '\n')
+			*st = '\0';
 		else
 			while (getchar() != '\n')
 				continue;
