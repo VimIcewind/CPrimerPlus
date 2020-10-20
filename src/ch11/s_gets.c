798d62d45ac6d835c3c21efb8041fb37c5a821d5
VimIcewind <VimIcewind@gmail.com>
2020-10-20 16:24:02 +0800

    s_gets() use strchr()

--- src/ch11/s_gets.c
+++ src/ch11/s_gets.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
+#include <string.h>
 
 char *s_gets(char *st, int n)
 {
 	char *ret_val;
+	char *find;
 
 	ret_val = fgets(st, n, stdin);
 	if (ret_val) {
-		while (*st != '\n' && *st != '\0')
-			st++;
-		if (*st == '\n')
-			*st = '\0';
+		find = strchar(st, '\n');
+		if (find)
+			*find = '\0';
 		else
 			while (getchar() != '\n')
 				continue;
