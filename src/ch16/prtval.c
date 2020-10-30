2dbc0ec2f71a2d2809d15d2873c2b9eefdb5ce7c
VimIcewind <VimIcewind@gmail.com>
2020-10-30 15:08:14 +0800

    Add \n

--- src/ch16/prtval.c
+++ src/ch16/prtval.c
@@ -1,7 +1,7 @@
 // prtval.c -- 使用宏函数来打印两个表达式的值
 #include <stdio.h>
 
-#define PRTVAL(X, Y) printf(#X " is %d and " #Y " is %d", (X), (Y))
+#define PRTVAL(X, Y) printf(#X " is %d and " #Y " is %d\n", (X), (Y))
 
 int main(void)
 {
